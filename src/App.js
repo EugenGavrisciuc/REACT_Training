50aeb325494debb2b386e0467c64450eed56640e
n0moreCRY <EugenGavrisciuc@yandex.ru>
2022-01-25 15:34:43 +0200

    REACT Training 3

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, {useState} from "react";
 import ClassCounter from "./components/ClassCounter";
 import Counter from "./components/Counter";
 import PostItem from "./components/PostItem";
+import PostList from "./components/PostList"
 import "./styles/App.css";
 
 function App() {
@@ -12,14 +13,18 @@ function App() {
 		{id: 3, title:"Javascript 3", body: "Description 3"}
 	])
 
+	const [posts2, statePosts2] = useState([
+		{id: 1, title:"Python", body: "Description"},
+		{id: 2, title:"Python 2", body: "Description 2"},
+		{id: 3, title:"Python 3", body: "Description 3"}
+	])
+
 	return (
 		<div className="App">
 			{/* <Counter/> */}
 			{/* <ClassCounter/> */}
-			<h1>Posts list</h1>
-			{posts.map(post =>
-				<PostItem post={post} key={post.id}/>
-				)}
+			{<PostList posts={posts} title="Posts about JS"/>}
+			{<PostList posts={posts2} title="Posts about Python"/>}
 		</div>
 	);
 }
