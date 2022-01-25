d64fa4104dfccd035c58f4ccde847348b5ca2554
n0moreCRY <EugenGavrisciuc@yandex.ru>
2022-01-25 12:18:52 +0200

    REACT Training 2

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
 import React, {useState} from "react";
 import ClassCounter from "./components/ClassCounter";
 import Counter from "./components/Counter";
+import PostItem from "./components/PostItem";
+import "./styles/App.css";
 
 function App() {
 
-	const [value, setValue] = useState("Input value");
+	const [posts, statePosts] = useState([
+		{id: 1, title:"Javascript", body: "Description"},
+		{id: 2, title:"Javascript 2", body: "Description 2"},
+		{id: 3, title:"Javascript 3", body: "Description 3"}
+	])
 
 	return (
 		<div className="App">
 			{/* <Counter/> */}
-			<ClassCounter/>
+			{/* <ClassCounter/> */}
+			<h1>Posts list</h1>
+			{posts.map(post =>
+				<PostItem post={post} key={post.id}/>
+				)}
 		</div>
 	);
 }
